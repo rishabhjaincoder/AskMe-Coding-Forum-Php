f7101f68ede74d9c49b633948ec1d63c9f2448ed
rishabhjaincoder <rishabhjain.rdps@gmail.com>
2021-04-15 20:52:03 +0530

    created threadlist.php and added dummy js forum with dummy threads

---
+++ threadlist.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@400;500&display=swap" rel="stylesheet">
+    <title>About - Coding Forum</title>
+    <style>
+    * {
+        font-family: 'Baloo Bhai 2', cursive;
+    }
+    </style>
+</head>
+
+<body>
+    <?php include 'partials/_header.php'; ?>
+    <?php include 'partials/_dbconnect.php'; ?>
+
+    <!-- ------------------- Jumbotron ------------------- -->
+    <div class="container-fluid bg-dark text-light my-0 py-3 px-5">
+        <div class="jumbotron-fluid">
+            <h1 class="display-4">Welcome to JavaScript Forums!</h1>
+            <p class="lead">JavaScript, often abbreviated as JS, is a programming language that conforms to the
+                ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm.</p>
+            <hr class="my-4">
+            <p>This is a peer to peer forum. No Spam / Advertising / Self-promote in the forums is not allowed. Do not post copyright-infringing material. Do not post “offensive” posts, links or images. Remain respectful of other members at all times</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+        </div>
+    </div>
+
+    <!-- ------------------- Browse Questions ------------------- -->
+    <div class="container text-center my-4">
+        <h2 class="my-4">AskMe - Browse Questions</h2>
+        <hr>
+    </div>
+    <div class="container my-4 px-5">
+        <div class="media row my-3">
+            <div class="col-12 col-md-1 d-flex justify-content-center">
+                <img class="mr-3" width="60px" height="60px" src="img/user-default/user.png" alt="Generic placeholder image">
+            </div>
+            <div class="col-12 col-md-11">
+                <div class="media-body">
+                    <h5 class="mt-3 mt-md-0">Pandas replace function not working on datafram with floats</h5>
+                    I am using Pandas' replace function and it works on one DataFrame but not on another and I don't understand why. I have tried many different solutions but nothing seems to work 
+                </div>
+            </div>
+        </div>
+        <div class="media row my-3">
+            <div class="col-12 col-md-1 d-flex justify-content-center">
+                <img class="mr-3" width="60px" height="60px" src="img/user-default/user.png" alt="Generic placeholder image">
+            </div>
+            <div class="col-12 col-md-11">
+                <div class="media-body">
+                    <h5 class="mt-3 mt-md-0">Pandas replace function not working on datafram with floats</h5>
+                    I am using Pandas' replace function and it works on one DataFrame but not on another and I don't understand why. I have tried many different solutions but nothing seems to work 
+                </div>
+            </div>
+        </div>
+        <div class="media row my-3">
+            <div class="col-12 col-md-1 d-flex justify-content-center">
+                <img class="mr-3" width="60px" height="60px" src="img/user-default/user.png" alt="Generic placeholder image">
+            </div>
+            <div class="col-12 col-md-11">
+                <div class="media-body">
+                    <h5 class="mt-3 mt-md-0">Pandas replace function not working on datafram with floats</h5>
+                    I am using Pandas' replace function and it works on one DataFrame but not on another and I don't understand why. I have tried many different solutions but nothing seems to work 
+                </div>
+            </div>
+        </div>
+        <div class="media row my-3">
+            <div class="col-12 col-md-1 d-flex justify-content-center">
+                <img class="mr-3" width="60px" height="60px" src="img/user-default/user.png" alt="Generic placeholder image">
+            </div>
+            <div class="col-12 col-md-11">
+                <div class="media-body">
+                    <h5 class="mt-3 mt-md-0">Pandas replace function not working on datafram with floats</h5>
+                    I am using Pandas' replace function and it works on one DataFrame but not on another and I don't understand why. I have tried many different solutions but nothing seems to work 
+                </div>
+            </div>
+        </div>
+        <div class="media row my-3">
+            <div class="col-12 col-md-1 d-flex justify-content-center">
+                <img class="mr-3" width="60px" height="60px" src="img/user-default/user.png" alt="Generic placeholder image">
+            </div>
+            <div class="col-12 col-md-11">
+                <div class="media-body">
+                    <h5 class="mt-3 mt-md-0">Pandas replace function not working on datafram with floats</h5>
+                    I am using Pandas' replace function and it works on one DataFrame but not on another and I don't understand why. I have tried many different solutions but nothing seems to work 
+                </div>
+            </div>
+        </div>
+        <div class="media row my-3">
+            <div class="col-12 col-md-1 d-flex justify-content-center">
+                <img class="mr-3" width="60px" height="60px" src="img/user-default/user.png" alt="Generic placeholder image">
+            </div>
+            <div class="col-12 col-md-11">
+                <div class="media-body">
+                    <h5 class="mt-3 mt-md-0">Pandas replace function not working on datafram with floats</h5>
+                    I am using Pandas' replace function and it works on one DataFrame but not on another and I don't understand why. I have tried many different solutions but nothing seems to work 
+                </div>
+            </div>
+        </div>
+        <div class="media row my-3">
+            <div class="col-12 col-md-1 d-flex justify-content-center">
+                <img class="mr-3" width="60px" height="60px" src="img/user-default/user.png" alt="Generic placeholder image">
+            </div>
+            <div class="col-12 col-md-11">
+                <div class="media-body">
+                    <h5 class="mt-3 mt-md-0">Pandas replace function not working on datafram with floats</h5>
+                    I am using Pandas' replace function and it works on one DataFrame but not on another and I don't understand why. I have tried many different solutions but nothing seems to work 
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ------------------- Footer ------------------- -->
+    <?php include 'partials/_footer.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"
+        integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"
+        integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous">
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script> -->
+</body>
+
+</html>
